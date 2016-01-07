201835e86a484cf9ee948fcc925a20501aeaa280
Vsevolod <gsevka@gmail.com>
2016-01-07 22:36:24 +0800

    Add validation for IPv6 address. Fixed #3 issue
    Conflicts:
    	glebtv_ipgeobase.gemspec
    	lib/ipgeobase.rb
    	lib/ipgeobase/version.rb

--- lib/ipgeobase.rb
+++ lib/ipgeobase.rb
@@ -1,5 +1,6 @@
 require "ipgeobase/version"
 require 'ipaddr'
+require "resolv"
 
 if defined? Rails::Engine
   require 'ipgeobase/engine'
@@ -8,6 +9,7 @@ end
 module Ipgeobase
   class << self
     def find_region_by_ip(user_ip)
+      raise ArgumentError, "Must have a valid IPv4 or IPv6 addres" unless !!((user_ip =~ Resolv::IPv4::Regex) || (user_ip =~ Resolv::IPv6::Regex))
       long = ip2long(user_ip)
       Ipgeobase::Ip.select( <<-SELECT ).where( "start_ip <= #{long}" ).order( 'start_ip DESC' ).first.try(:city)
         CASE WHEN #{long} <= end_ip
